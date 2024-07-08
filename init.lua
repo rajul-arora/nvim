7a88423e8eb486a02023d8a444323c01aa94ba1b
Rajul Arora <rajul@fitbod.me>
2024-07-08 11:34:08 -0400

    Add Lualine and config

--- init.lua
+++ init.lua
@@ -19,17 +19,27 @@ vim.opt.rtp:prepend(lazypath)
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
+
+local keybindings = {
+  { mode = "n", key = "<leader>h", desc = "Toggle help overlay" },
+  { mode = "n", key = "<leader>ff", desc = "Toggle Find Files"},
+  { mode = "n", key = "<leader>fg", desc = "Toggle Live Grep"},
+  { mode = "n", key = "Ctrl + w + Arrow Key", desc = "Switch between splits"}
+}
+
 require("lazy").setup({
   "folke/which-key.nvim",
   "Shatur/neovim-ayu",
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
   {
     'nvim-help',
     dir = '~/.config/nvim/nvim-help',
     config = function()
       require('nvim-help').setup({
-        keybindings = {
-          { mode = "n", key = "<leader>h", desc = "Toggle help overlay" },
-        }
+        keybindings = keybindings
       })
       vim.api.nvim_set_keymap('n', '<leader>h', ':lua require("nvim-help").toggle_help()<CR>', { noremap = true, silent = true })
     end, 
@@ -84,4 +94,51 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+---------------------------
+-- Lualine Configuration --
+---------------------------
+
+local lualine = require('lualine')
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'ayu_dark',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
 
